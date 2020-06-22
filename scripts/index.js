36137361d9c1328e4bf163ffbfb8c30db8daaec7
Aleksey Kozlovskiy <aikozlovsky@yandex.ru>
2020-06-22 22:58:58 +1000

    fix: Исправь адаптивный профиль, тэги кнопок

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,31 @@
 let content = document.querySelector('.content');
 let editButton = content.querySelector('.edit-button');
 let popup = content.querySelector('.popup');
-let closePopup = popup.querySelector('.close-icon');
+let closeButton = popup.querySelector('.close-button');
+let profileName = content.querySelector('.profile__name');
+let profileProfession = content.querySelector('.profile__profession');
+let inputName = popup.querySelector('.popup__item_el_name');
+let inputProfession = popup.querySelector('.popup__item_el_profession');
 
-function openPopup() {
+function closePopup() {
     popup.classList.toggle('popup_opened')
     popup.classList.toggle('popup_closed')
 }
 
+function openPopup() {
+    inputName.value = profileName.textContent;
+    inputProfession.value = profileProfession.textContent;
+    closePopup();
+}
+
 editButton.addEventListener('click', openPopup);
-closePopup.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    let profileName = content.querySelector('.profile__name');
-    let profileProfession = content.querySelector('.profile__profession');
-    let inputName = popup.querySelector('.popup__item_el_name');
-    let inputProfession = popup.querySelector('.popup__item_el_profession');
     profileName.textContent = inputName.value;
     profileProfession.textContent = inputProfession.value;
-    openPopup();
+    closePopup();
 }
 
 popup.addEventListener('submit', formSubmitHandler);
