e929fce55344a8a5075f048aecf057812c9f8b91
Aleksey Kozlovskiy <aikozlovsky@yandex.ru>
2020-07-05 13:35:56 +1000

    feat: Добавь загрузку первоначальных карточек из JS

--- scripts/index.js
+++ scripts/index.js
@@ -22,6 +22,56 @@ function formSubmitHandler (evt) {
     openClosePopup();
 }
 
+const cardsListElement = document.querySelector(".elements__grid")
+
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+function addCard(cardData) {
+    const card = `
+    <li class="element">
+    <img src="${cardData.link}" alt="Фото карточки" class="element__image">
+    <div class="element__info">
+        <h3 class="element__heading">
+            ${cardData.name}
+        </h3>
+        <button type="button" class="element__like-button">
+            <img src="./images/Element__like.svg" class="element__like-image" alt="Лайк">
+        </button>
+    </div>
+</li>
+    `
+    cardsListElement.insertAdjacentHTML('afterbegin', card);
+}
+
+initialCards.forEach(cardData => {
+    addCard(cardData);
+});
+
 popup.addEventListener('submit', formSubmitHandler);
 editButton.addEventListener('click', openClosePopup);
 closeButton.addEventListener('click', openClosePopup);
